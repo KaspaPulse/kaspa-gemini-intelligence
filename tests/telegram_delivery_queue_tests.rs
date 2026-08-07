ae7239e0ed6afde4a99dcf66089fef00ed839cfa
KaspaPulse <KaspaPulse@gmail.com>
2026-08-08 01:34:20 +0300

    test: remove process-wide env mutation under Rust 2024

--- tests/telegram_delivery_queue_tests.rs
+++ tests/telegram_delivery_queue_tests.rs
@@ -1,42 +1,6 @@
 use kaspa_pulse::infrastructure::telegram_delivery_queue::{
-    delivery_queue_enabled, retry_after_seconds, retry_delay_seconds,
+    retry_after_seconds, retry_delay_seconds,
 };
-use std::sync::Mutex;
-
-static ENV_TEST_LOCK: Mutex<()> = Mutex::new(());
-
-#[test]
-fn telegram_delivery_queue_enabled_by_default() {
-    let _guard = ENV_TEST_LOCK.lock().expect("env test lock poisoned");
-
-    std::env::remove_var("ENABLE_TELEGRAM_DELIVERY_QUEUE");
-    assert!(delivery_queue_enabled());
-}
-
-#[test]
-fn telegram_delivery_queue_can_be_disabled() {
-    let _guard = ENV_TEST_LOCK.lock().expect("env test lock poisoned");
-
-    std::env::remove_var("ENABLE_TELEGRAM_DELIVERY_QUEUE");
-    std::env::set_var("ENABLE_TELEGRAM_DELIVERY_QUEUE", "false");
-
-    assert!(!delivery_queue_enabled());
-
-    std::env::remove_var("ENABLE_TELEGRAM_DELIVERY_QUEUE");
-}
-
-#[test]
-fn telegram_delivery_queue_accepts_enabled_values() {
-    let _guard = ENV_TEST_LOCK.lock().expect("env test lock poisoned");
-
-    std::env::set_var("ENABLE_TELEGRAM_DELIVERY_QUEUE", "enabled");
-    assert!(delivery_queue_enabled());
-
-    std::env::set_var("ENABLE_TELEGRAM_DELIVERY_QUEUE", "1");
-    assert!(delivery_queue_enabled());
-
-    std::env::remove_var("ENABLE_TELEGRAM_DELIVERY_QUEUE");
-}
 
 #[test]
 fn retry_after_is_extracted_from_telegram_error() {
